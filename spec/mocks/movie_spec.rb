ccf7fa9e07436fc2611bfe95ab3dc6e38c6043ce
Michael Reeves <111369642+michael-reeves-nc@users.noreply.github.com>
2022-11-15 17:59:50 -0700

    52: Replacing an Object with a Double

--- spec/mocks/movie_spec.rb
+++ spec/mocks/movie_spec.rb
@@ -37,7 +37,29 @@ class Movie
   end
 end
 
-actor = Actor.new('Brad Pitt')
-movie = Movie.new(actor)
+# actor = Actor.new('Brad Pitt')
+# movie = Movie.new(actor)
+#
+# movie.start_shooting
 
-movie.start_shooting
+RSpec.describe Movie do
+  let(:stuntman) do
+    double("Mr. Danger", ready?: true,
+                         act:    "Any string at all",
+                         fall_off_ladder: "Sure! Let's do it",
+                         light_on_fire: true)
+  end
+
+  subject { described_class.new(stuntman) }
+
+  describe '#start_shooting method' do
+    it 'expects an actor to do 3 actions' do
+      expect(stuntman).to receive :ready?
+      expect(stuntman).to receive :act
+      expect(stuntman).to receive :fall_off_ladder
+      expect(stuntman).to receive :light_on_fire
+
+      subject.start_shooting
+    end
+  end
+end
